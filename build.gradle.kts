8f18240c6ab709222f8a0af47b29d90847d922c5
Christian Dräger <christian.draeger1@gmail.com>
2022-03-15 15:23:00 +0100

    build: create test coverage report on test execution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA_PARALLEL
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -5,6 +6,8 @@ plugins {
     id("io.spring.dependency-management") apply false
     kotlin("jvm")
     kotlin("plugin.spring") apply false
+    id("com.adarshr.test-logger")
+    id("org.jetbrains.kotlinx.kover")
     // id("org.springframework.experimental.aot") apply false
 }
 
@@ -17,6 +20,12 @@ allprojects {
         mavenCentral()
     }
 
+    apply(plugin = "com.adarshr.test-logger")
+    testlogger {
+        theme = MOCHA_PARALLEL
+        slowThreshold = 1000
+        showStandardStreams = false
+    }
 }
 
 subprojects {
@@ -36,5 +45,15 @@ subprojects {
         withType<Test> {
             useJUnitPlatform()
         }
+    }
+}
+
+kover {
+    coverageEngine.set(kotlinx.kover.api.CoverageEngine.INTELLIJ)
+}
+
+tasks {
+    test {
+        finalizedBy(koverReport, koverCollectReports)
     }
 }
