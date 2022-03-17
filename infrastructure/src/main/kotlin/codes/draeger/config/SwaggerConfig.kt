85ecf843f7b1ff922f2c8c171b562d975e231aae
Christian Dräger <christian.draeger1@gmail.com>
2022-03-17 19:50:46 +0100

    feat: simplify swagger config

--- infrastructure/src/main/kotlin/codes/draeger/config/SwaggerConfig.kt
+++ infrastructure/src/main/kotlin/codes/draeger/config/SwaggerConfig.kt
@@ -4,27 +4,12 @@ import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
-import org.springdoc.core.GroupedOpenApi
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SwaggerConfig(private val buildProperties: BuildProperties) {
-
-    @Bean
-    fun userApis(): GroupedOpenApi {
-        return GroupedOpenApi.builder().group("web")
-            .pathsToMatch("/api/info/v1/**")
-            .build()
-    }
-
-    @Bean
-    fun adminApis(): GroupedOpenApi {
-        return GroupedOpenApi.builder().group("android app")
-            .pathsToMatch("/android/v1/**")
-            .build()
-    }
 
     @Bean
     fun customOpenAPI(): OpenAPI {
